8a8643f7ed4a8b431eae57bde7d630d9fa55c47f
Raymond Gigliotti <ray@firestitch.com>
2022-07-14 10:32:19 -0400

    added fsclear and fsstore

--- src/app/interfaces/config.interface.ts
+++
@@ -1,5 +0,0 @@
-import { editor } from 'monaco-editor';
-
-export interface FsTextEditorConfig extends editor.IStandaloneEditorConstructionOptions {
-  autoHeight?: boolean
-}
--- src/public_api.ts
+++ src/public_api.ts
@@ -1,4 +1,6 @@
 export { FsPdfViewerModule } from './app/modules/pdf-viewer/fs-pdf-viewer.module';
 
-export { FsTextEditorConfig } from './app/interfaces/config.interface';
+export { FsPdfViewerComponent } from './app/modules/pdf-viewer/components/pdf-viewer/pdf-viewer.component';
+
+export { FsPdfFormComponent } from './app/modules/pdf-form/components/pdf-form/pdf-form.component';
 
