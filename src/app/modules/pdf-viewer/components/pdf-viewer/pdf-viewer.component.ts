51e1d506117aa887aff2160a9167a66232cea952
Deployment Manager <sysadmin@firestitch.com>
2024-10-05 11:57:15 +0000

    Angular 17 Upgrade

--- src/app/modules/pdf-viewer/components/pdf-viewer/pdf-viewer.component.ts
+++ src/app/modules/pdf-viewer/components/pdf-viewer/pdf-viewer.component.ts
@@ -77,7 +77,7 @@ export class FsPdfViewerComponent implements OnInit, OnDestroy {
   }
 
   public ngOnDestroy(): void {
-    this._destroy$.next();
+    this._destroy$.next(null);
     this._destroy$.complete();
   }
 
